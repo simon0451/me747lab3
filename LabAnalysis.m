1ba6e4e7c2b6af5f6c1833ae01e4155ea6221eb4
ZhangxiJesseFeng <zf1005@wildcats.unh.edu>
2017-10-27 01:23:15 -0400

    made progress

--- LabAnalysis.m
+++ LabAnalysis.m
@@ -7,17 +7,21 @@ lvtdata = importdata('LVT v(t).lvm','\t',29);
 lvtT = lvtdata.data(:,1);
 lvtV = lvtdata.data(:,2);
 
-% lvtP = cumtrapz(lvtT,lvtV);
+lvtT = lvtT(1:1500);
+lvtV = lvtV(1:1500);
+
+lvtP = cumtrapz(lvtT,lvtV);
+lvtP = lvtP - mean(lvtP(1200:end));
 
 % 1 a) Plot LVT Output Voltage vs. Time
 figure(1);
-% yyaxis left;
+yyaxis left;
 plot(lvtT,lvtV);
 ylabel('Voltage (V)');
-% yyaxis right;
-% plot(lvtT,lvtP);
+yyaxis right;
+plot(lvtT,lvtP);
 xlabel('Time (s)');
-% ylabel('Integrated V (Vs)');
+ylabel('Integrated V (Vs)');
 
 % Observation: Mass is free falling (linear slope) for the first 3 times
 % i) first 200 data points are initial stuff
